a33b5580c4aea9eb4ae564dceaa8aca8f07908f7
kerami <keramiozsoy@hotmail.com>
2019-05-22 18:48:24 +0300

    configserver

---
+++ configserver/src/main/java/com/microservice/demo/configserver/ConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+}
