921f4e095debb412dfa391add41910f9daa94e2c
kerami <keramiozsoy@hotmail.com>
2019-05-28 19:57:55 +0300

    @enableeurekaclient

--- client/src/main/java/com/microservice/demo/client/ClientApplication.java
+++ client/src/main/java/com/microservice/demo/client/ClientApplication.java
@@ -1,7 +1,6 @@
 package com.microservice.demo.client;
 
 import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
