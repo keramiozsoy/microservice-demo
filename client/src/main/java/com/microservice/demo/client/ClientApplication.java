80c8a608eb6beb1c7406bee0ca8cf45618fe90ad
kerami <keramiozsoy@hotmail.com>
2019-05-30 18:23:54 +0300

    config

--- client/src/main/java/com/microservice/demo/client/ClientApplication.java
+++ client/src/main/java/com/microservice/demo/client/ClientApplication.java
@@ -3,7 +3,6 @@ package com.microservice.demo.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +18,6 @@ public class ClientApplication {
 }
 
 @RestController
-@RefreshScope
 class CustomClientController {
 
 	@Value("${msg : can not retrieve msg ... configserver can not running....}")
