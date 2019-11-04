5f0712ca65c91c54e5d1895700e3cc6a8dd5f87d
kerami <keramiozsoy@hotmail.com>
2019-11-04 22:14:04 +0300

    client2 project initialized

---
+++ client2/src/test/java/com/microservice/demo/client2/ClientApplicationTests2.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.client2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests2 {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
